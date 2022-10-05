1a228ced85c724eb4da5bf19b8bd2d1fbbcf38c6
scary3410 <98115985+scary3410@users.noreply.github.com>
2022-10-05 18:19:28 +0530

    Update file to text.py

--- file to text.py
+++ file to text.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import pywhatkit as kit
 
-ab= open("tet.txt","r")
+ab= open("Write-Here.txt","r")
 info=ab.read()
 kit.text_to_handwriting(info,save_to="handwriting.png")
 Image.open("clcoding.png")
